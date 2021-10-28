fc5e8679ab2f581d9a7bc870e91c1701acf4e6c8
Pablo Pulisich <pablopuli94@gmail.com>
2021-10-28 19:44:27 -0300

    Navbar puesta

---
+++ src/components/title/title.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class TitleComponent extends Component{
+
+    render(){
+        return <div>
+            <h1>¡Hola Mundo!</h1>
+        </div>
+    }
+}
+
+export default TitleComponent;
