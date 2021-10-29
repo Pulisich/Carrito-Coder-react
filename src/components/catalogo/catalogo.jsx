03c10d3b91c1d0551f49bc80696c4d9f58b10ed8
Pablo Pulisich <pablopuli94@gmail.com>
2021-10-29 15:22:29 -0300

    Navbar custom botones y texto, falta color fuente y alineado

---
+++ src/components/catalogo/catalogo.jsx
@@ -0,0 +1,18 @@
+import {Component} from 'react';
+
+class Catalogo extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: 'Gonzalo';
+        }        
+    }
+
+    render(){
+        return
+            <div>
+                
+            </div>;
+        
+    }
+}
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -12,19 +12,14 @@ export default function ButtonAppBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            CityPhone
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit">Fundas</Button>
+          <Button color="inherit">Vidrios templados</Button>
+          <Button color="inherit">Cargadores</Button>
+          <Button color="inherit">Auriculares</Button>
+          <Button color="inherit">Accesorios</Button>
         </Toolbar>
       </AppBar>
     </Box>
