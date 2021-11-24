3d129693fa6c6cc488fb4f30aec584e1a2d5441f
Pablo Pulisich <pablopuli94@gmail.com>
2021-11-23 21:04:05 -0300

    Agregadas imágenes

--- src/components/ItemListContainer/Item.jsx
+++ src/components/ItemListContainer/Item.jsx
@@ -5,13 +5,22 @@ import { Fragment } from "react"
 const Item = ({product}) =>{
     console.log("Item: ", product)
 
+    
+
     return(
         <Fragment>
-            <h1>{product.id}</h1>
+           <div className="productos">
+
+           <h1>{product.id}</h1>
+            
             <h2>{product.name}</h2>
+            
+            <img src={product.img} alt="" srcset="" />
+           
             <h3>{product.price}</h3>
             <h3>{product.category}</h3>
             <h3>{product.stock}</h3>
+           </div>
         </Fragment>
         
     )
